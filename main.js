0bd430ab552a278fb99a22889ac6648beb1e3960
akepoguvijay <85406979+akepoguvijay@users.noreply.github.com>
2022-02-28 20:49:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(400,350);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('clownFilterapp.png');
+}
+
